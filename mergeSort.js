f8078b26a34eb6c55acc799bd095f7601d1bb563
Gita Linkeviciute <gita.link@gmail.com>
2019-12-10 18:01:24 -0500

    Add mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -2,5 +2,68 @@ function split(wholeArray){
   const middle=Math.floor(wholeArray.length/2);
   const firstHalf=wholeArray.slice(0, middle)
   const secondHalf=wholeArray.slice(middle)
+  // console.log('FIRST HALF', firstHalf)
+  // console.log('SECOND HALF', secondHalf)
   return [firstHalf, secondHalf]
 }
+
+function merge(sortedArr1, sortedArr2) {
+  if (sortedArr1.length > sortedArr2.length) {
+    arr1 = sortedArr1;
+    arr2 = sortedArr2;
+  }
+  else {
+    arr1 = sortedArr2;
+    arr2 = sortedArr1;
+  }
+
+  const mergedArr = [] ;
+  let k = 0;
+    for (i=0; i<arr1.length; i++) {
+      let arr1Element = arr1[i]
+      for (j=k; j<arr2.length; j++) {
+        let arr2Element = arr2[j]
+        if (arr1Element > arr2Element) {
+          mergedArr.push(arr2Element)
+          k++;
+        }
+        else {
+          break;
+        }
+      }
+      mergedArr.push(arr1Element)
+    }
+    // console.log('   Merged Arr',mergedArr)
+  return mergedArr
+}
+
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array
+  }
+  const splitArr = split(array)
+  let arr1=mergeSort(splitArr[0])
+  let arr2=mergeSort(splitArr[1])
+  console.log('   SplitArray1',arr1)
+  console.log('   SplitArray2',arr2)
+  mergedArray=merge(arr1,arr2)
+  console.log('   Merged Arr',mergedArray)
+  return mergedArray
+
+}
+
+
+// function merge2(arr1, arr2) {
+//   const mergedArr = [] ;
+
+//   while (arr1.length && arr2.length) {
+//     if(arr1[0] < arr2[0]) {
+//       mergedArr.push(arr1.pop())
+//     }
+//     else if (arr1[0] > arr2[0]) {
+//       mergedArr.push(arr2.shift())
+//     }
+//   }
+
+//   return mergedArr
+// }
--- mergeSort.spec.js
+++ mergeSort.spec.js
@@ -2,9 +2,27 @@
 
 describe('Split Array function ', function(){
   it('is able to split an array into two halves', function(){
-    const array=[2,3,4,5,1];
+    const array=[1,2,4,5];
     const splitArray=split(array);
     expect(splitArray[0].length).toEqual(2);
-    expect(splitArray[1].length).toEqual(3);
+    expect(splitArray[1].length).toEqual(2);
+  })
+})
+
+describe('Merge array function', function() {
+  it ('is able to merge two sorted arrays into one sorted array', function() {
+    const sortedArr1 = [2, 3, 6];
+    const sortedArr2 = [1, 4, 5];
+
+
+
+    expect(merge(sortedArr1, sortedArr2)).toEqual([1, 2, 3, 4, 5])
+  })
+})
+
+describe('Merge sort function', function() {
+  it ('is sorts unsorted array', function() {
+    const array=[2,3,4,5,1];
+    expect(mergeSort(array)).toEqual([1, 2, 3, 4, 5])
   })
 })
